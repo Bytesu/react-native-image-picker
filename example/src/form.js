0c5651d20ef03df520807db29add054c9f84feb8
byte_su <byte_su@163.com>
2020-12-08 11:17:40 +0800

    上划单选

--- example/src/form.js
+++ example/src/form.js
@@ -20,8 +20,25 @@ export default function App() {
     },
     {
       label: '籍贯', name: 'birth',
-      value: '河南',
+      value: '',
       type: FormType.SELECT_FROM_PAGE,
+      options: [
+        {value: '河南', label: '河南'},
+        {value: '山西', label: '山西'},
+        {value: '陕西', label: '陕西'},
+        {value: '陕西1', label: '陕西'},
+        {value: '陕西2', label: '陕西'},
+        {value: '陕西3', label: '陕西'},
+        {value: '陕西4', label: '陕西'},
+        {value: '陕西6', label: '陕西'},
+        {value: '陕西5', label: '陕西'},
+        {value: '陕西7', label: '陕西'},
+        {value: '陕西8', label: '陕西'},
+        {value: '陕西9', label: '陕西'},
+        {value: '陕西10', label: '陕西10'},
+        {value: '陕西11', label: '陕西11'},
+        {value: '陕西12', label: '陕西12'},
+      ],
     },
     [
       {
--- src/style/var.js
+++ src/style/var.js
@@ -2,4 +2,6 @@ export default {
   titleColor: '#333', //标题颜色
   pageColor: '#fff', //页面背景色
   headerColor: '#eee', //标题背景色
+  btnActiveColor: '#eee', //按钮选中后颜色
+  itemActiveColor: 'rgba(238,238,238,.5)', //列表项选中后颜色
 };
